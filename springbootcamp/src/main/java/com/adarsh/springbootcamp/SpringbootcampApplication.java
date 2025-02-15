611efff84039be61f7a6922d1f52d018ffd134ed
adarsh srivastav <1adarsh777@gmail.com>
2025-02-15 23:58:46 +0530

    First commit

---
+++ springbootcamp/src/main/java/com/adarsh/springbootcamp/SpringbootcampApplication.java
@@ -0,0 +1,14 @@
+package com.adarsh.springbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcampApplication.class, args);
+		System.out.println("****** PROJECT STARTED ******");
+	}
+
+}
